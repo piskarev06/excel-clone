8332c8cf04301bef2957003096a2738f2a2e3f3f
piskarev06 <msabirova34@gmail.com>
2021-11-02 12:59:58 +0300

    Finish Excel logic

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,17 +1,23 @@
 import {$} from '@core/dom'
+import {Emitter} from '@core/Emitter'
 
 export class Excel {
   constructor(selector, options) {
     this.$el = $(selector)
     this.components = options.components || []
+    this.emitter = new Emitter()
   }
 
   getRoot() {
     const $root = $.create('div', 'excel')
 
+    const componentOptions = {
+      emitter: this.emitter
+    }
+
     this.components = this.components.map(Component => {
       const $el = $.create('div', Component.className)
-      const component = new Component($el)
+      const component = new Component($el, componentOptions)
       // // DEBUG
       // if (component.name) {
       //   window['c' + component.name] = component
@@ -28,5 +34,9 @@ export class Excel {
     this.$el.append(this.getRoot())
 
     this.components.forEach(component => component.init())
+  }
+
+  destroy() {
+    this.components.forEach(component => component.destroy())
   }
 }
